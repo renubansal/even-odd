cb63bd8223c185a3f3f6b7d4f086a1838f968025
Renu <bansal2407@gmail.com>
2022-06-29 19:31:27 +0530

    Add dummy store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.js";
 import "./index.css";
 
+import { configureStore } from "@reduxjs/toolkit";
 
+const rootReducer = () => {
+  return { foo: "bar" };
+};
+
+const store = configureStore({ reducer: rootReducer });
+
+console.log("Store:", store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
